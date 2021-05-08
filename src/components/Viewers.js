09181709e0534a43f6de73c2cbf3497358853d04
Alex Petroski <alexpetroski@gmail.com>
2021-05-07 21:09:58 -0400

    Added videos to background on hover for viewers

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -4,24 +4,29 @@ import styled from 'styled-components';
 function Viewers() {
   const viewers = [
     {
-      src: 'images/viewers-disney.png',
+      src: '/images/viewers-disney.png',
       alt: 'Disney Viewer Image',
+      video: '/videos/1564674844-disney.mp4',
     },
     {
       src: '/images/viewers-pixar.png',
       alt: 'Pixar Viewer Image',
+      video: '/videos/1564676714-pixar.mp4',
     },
     {
       src: '/images/viewers-marvel.png',
       alt: 'Marvel Viewer Image',
+      video: '/videos/1564676115-marvel.mp4',
     },
     {
       src: '/images/viewers-starwars.png',
       alt: 'Star Wars Viewer Image',
+      video: '/videos/1608229455-star-wars.mp4',
     },
     {
       src: '/images/viewers-national.png',
       alt: 'National Geographic Viewer Image',
+      video: '/videos/1564676296-national-geographic.mp4',
     },
   ];
 
@@ -31,6 +36,9 @@ function Viewers() {
         return (
           <Wrap>
             <img src={viewer.src} alt={viewer.alt} />
+            <video autoPlay={true} loop={true} playsInline={true}>
+              <source src={viewer.video} type='video/mp4' />
+            </video>
           </Wrap>
         );
       })}
@@ -46,18 +54,43 @@ const Container = styled.div`
   display: grid;
   grid-gap: 25px;
   grid-template-columns: repeat(5, minmax(0, 1fr));
+
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(1, minmax(0, 1fr));
+  }
 `;
 
 const Wrap = styled.div`
+  padding-top: 56.25%;
   border-radius: 10px;
-  cursor: pointer;
-  border: 3px solid rgba(249, 249, 249, 0.1);
   box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
     rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+  cursor: pointer;
+  overflow: hidden;
+  position: relative;
   transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+  border: 3px solid rgba(249, 249, 249, 0.1);
+
   img {
+    inset: 0px;
+    display: block;
+    height: 100%;
+    object-fit: cover;
+    opacity: 1;
+    position: absolute;
+    transition: opacity 500ms ease-in-out 0s;
+    width: 100%;
+    z-index: 1;
+    top: 0;
+  }
+
+  video {
     width: 100%;
     height: 100%;
+    position: absolute;
+    top: 0px;
+    opacity: 0;
+    z-index: 0;
     object-fit: cover;
   }
 
@@ -66,5 +99,9 @@ const Wrap = styled.div`
       rgb(0 0 0 / 72%) 0px 30px 22px -10px;
     transform: scale(1.05);
     border-color: rgba(249, 249, 249, 1);
+
+    video {
+      opacity: 1;
+    }
   }
 `;
